2df82481bdf9d5e9efc2f4644b7475969a8c3642
sikakoluchaitanya <sikakoluchaitaya@gmail.com>
2024-09-26 01:37:59 +0530

    added prisma schema

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExampleController } from './example/example.controller';
-import { ExampleService } from './example/example.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ExampleController],
-  providers: [AppService, ExampleService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

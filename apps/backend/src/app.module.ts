fd86367d12f7cecc020783199eb44d7fe13e71c6
sikakoluchaitanya <sikakoluchaitaya@gmail.com>
2024-09-26 16:51:41 +0530

    added chat feature in nestjs

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocketService } from './services/socket';
+
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SocketService],
 })
 export class AppModule {}

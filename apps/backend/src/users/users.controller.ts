63aab26302fc9e654b2277bf273e315164b19ae7
Suvan Sarkar <suvansarkar1@gmail.com>
2024-12-03 00:24:17 +0530

    Add MMR field to ValorantUser model and implement matchmaking logic in user service

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -13,17 +13,25 @@ export class UsersController {
 
   @Post('/connect')
   async connectUser(@Body() body: ConnectUserDto) {
+    console.log('[Nest] PSOT /users/connect');
     return this.usersService.connectUser(body);
   }
 
   @Get()
   async getAllUsers() {
     console.log('[Nest] GET /users');
-    return this.usersService.getAllUsers();
+    return await this.usersService.getAllUsers();
   }
 
   @Post('/ban/:id')
   async banUser(@Param('id') id: string) {
+    console.log('[Nest] POST /users/ban/' + id);
     return this.usersService.banUser(id);
+  }
+
+  @Get('/matchmaking/:id')
+  async getMatchmadeUser(@Param('id') id: string) {
+    console.log('[Nest] GET /matchmaking/' + id);
+    return await this.usersService.matchmakeValorant(id);
   }
 }
